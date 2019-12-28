b4c6b45588002f2e54b7f6dfaaffaeba2e37dc4a
Sujin Chaichanamongkol <sujinchaichanamongkol@gmail.com>
2019-12-28 22:24:21 +0700

    Initial Commit

---
+++ SkeletonTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonTest
+//
+//  Created by Sujin Chaichanamongkol on 28/12/2562 BE.
+//  Copyright © 2562 Sujin Chaichanamongkol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
